f7e115fe07111c186b80d1c7e9a8bbcae8361d8b
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-03-19 12:08:13 +0100

    fix(province_migration): column correction

--- database/migrations/2025_03_19_095500_create_provinces_table.php
+++ database/migrations/2025_03_19_095500_create_provinces_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('provinces', function (Blueprint $table) {
             $table->id();
-            $table->name('name');
+            $table->string('name');
             $table->timestamps();
         });
     }
