b8440318e37d8fd766cc1ec0ba40acd68df2805d
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-03-20 08:36:24 +0100

    feat(provinces): function api to array created

--- app/Models/Provinces.php
+++ app/Models/Provinces.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Http;
 
 class Provinces extends Model
 {
-    //
+    public function apiToArray(){
+        $response = json_decode(Http::get('https://servicios.ine.es/wstempus/js/ES/VALORES_VARIABLE/70'));
+
+        $lista = [];
+
+        foreach($response as $element){
+            if(intval($element->Codigo) > 0 && $element->Codigo != ""){
+                $lista[] = str_replace(" - ", " ",implode(" ", array_reverse(explode(", ", $element->Nombre))));
+            }
+        }
+
+        return $lista;
+    }
 }
