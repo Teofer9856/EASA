5bd33822880e28423a349749b530ff41be55e186
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-03-21 13:54:45 +0100

    feat(controller): generalController, index function implemented

--- app/Http/Controllers/GeneralController.php
+++ app/Http/Controllers/GeneralController.php
@@ -3,16 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\Models\Clients;
-use Illuminate\Http\Request;
+use App\Models\Provinces;
 use Illuminate\Support\Facades\Schema;
 
 class GeneralController extends Controller
 {
     public function index(){
-        $clients_list = Clients::all();
+        $clients_list = $this->namesChange(Clients::all());
         $entity = Schema::getColumnListing('clients');
         $names_list = array_splice($entity, 0,6);
 
         return view('clients.index', compact(['clients_list', 'names_list']));
     }
+
+    public function namesChange($clients_list){
+        foreach($clients_list as $object){
+            $name_province = $object->province['name'];
+            $name_seller = $object->seller['name'];
+
+            $object->province_id = $name_province;
+            $object->seller_id = $name_seller;
+        }
+        return $clients_list;
+    }
+
 }
