949e2e60f2f778e6669f2a17a1a4967c9c68d7f3
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-03-21 10:48:27 +0100

    feat(clients_model): names hidden

--- app/Models/Clients.php
+++ app/Models/Clients.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Clients extends Model
 {
     use HasFactory;
+
+    /**
+     * Esconde campos de la tabla
+     */
+    protected $hidden = ['created_at', 'updated_at'];
 }
