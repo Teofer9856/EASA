89f42a681b30c2f370cef62e372835134c170157
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-03-25 16:00:39 +0100

    feat(client request): client request created with rules

---
+++ app/Http/Requests/StoreClientRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreClientRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|min: 5|max: 50',
+            'email' => 'required|unique:clients,email',
+            'zip_code' => 'required|min:5',
+            'province_id' => 'required',
+            'seller_id' => 'required'
+        ];
+    }
+}
