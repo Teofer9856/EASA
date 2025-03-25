1e4a677650bdfb2078b149865c884e633e9ed3fd
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-03-25 16:19:03 +0100

    fix(form errors): form errors implemented in the whole form

--- resources/views/components/client-form.blade.php
+++ resources/views/components/client-form.blade.php
@@ -5,7 +5,7 @@
         <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre</label>
         <input type="text" id="name" name="name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Lionel Messi" value="{{old('name')}}" required />
         @error('name')
-            <span>{{$message}}</span>
+            <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Oops!</span> {{$message}}</p>
         @enderror
     </div>
 
@@ -22,7 +22,7 @@
             <label for="zip_code" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Zip Code</label>
             <input type="text" id="zip_code" name="zip_code" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="XXXXX" value="{{old('zip_code')}}" required />
             @error('zip_code')
-                <span>{{$message}}</span>
+                <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Oops!</span> {{$message}}</p>
             @enderror
         </div>
 
@@ -34,7 +34,7 @@
                 @endforeach
             </select>
             @error('province_id')
-                <span>{{$message}}</span>
+                <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Oops!</span> {{$message}}</p>
             @enderror
         </div>
     </div>
@@ -46,7 +46,7 @@
             @endforeach
         </select>
         @error('province_id')
-            <span>{{$message}}</span>
+        <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Oops!</span> {{$message}}</p>
         @enderror
     </div>
     <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Guardar</button>
