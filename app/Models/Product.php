56a7da197abd58660587f585979a057f861491bf
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-03-26 13:19:00 +0100

    feat(filter headers): filter created on products model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,4 +9,12 @@ class Product extends Model
 {
     use HasFactory;
 
+    protected $hidden = ['created_at', 'updated_at'];
+
+    public static function fileteredNames(array $headers){
+        $filteredColumns = array_filter($headers, function($column) {
+           return !in_array($column, ['created_at', 'updated_at']);
+        });
+        return $filteredColumns;
+    }
 }
