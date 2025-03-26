79dbd23951b3786888a39776a16fd48818bee2a3
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-03-26 08:26:11 +0100

    feat(client form): form view completed

--- resources/views/components/client-form.blade.php
+++ resources/views/components/client-form.blade.php
@@ -1,3 +1,4 @@
+<a href="{{route('clients.index')}}" class="font-medium text-blue-500 dark:text-blue-500 hover:underline flex justify-end mr-10">Volver</a>
 <form class="max-w-md mx-auto" method="POST" action="{{route('clients.store')}}">
     @csrf
 
@@ -49,5 +50,14 @@
         <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Oops!</span> {{$message}}</p>
         @enderror
     </div>
-    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Guardar</button>
+    <div class="grid md:grid-cols-2 md:gap-6">
+        <div class="relative z-0 w-full mb-5 group">
+            <button type="reset" name="back" class="py-2.5 px-5 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 w-full hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Limpiar</button>
+        </div>
+
+        <div class="relative z-0 w-full mb-5 group">
+            <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Guardar</button>
+        </div>
+    </div>
+
 </form>
