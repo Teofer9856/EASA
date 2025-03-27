b42733215a1675214ab9711834dd604154d76351
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-03-27 08:24:32 +0100

    perf(table): now u can choose if u want to align the text from the begining or the center

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -1,47 +1,61 @@
-    <div>
-        <div class="relative overflow-x-auto">
-            <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 mb-3">
-                <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        @foreach ($headers as $value)
-                            @if ($value != 'id')
-                                <th scope="col" class="px-4 py-3 text-center">
-                                    {{$value}}
-                                </th>
-                            @else
-                                <th scope="col" class="px-4 py-3 text-center"></th>
-                            @endif
-                        @endforeach
-                        <th class="px-4 py-3 text-center">
-                            Action
-                        </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                        @foreach ($list as $value)
-                            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                                @foreach ($headers as $name)
-                                    @if ($name != 'id')
-                                        <td class="px-4 py-4 text-center">
-                                            {{($value->$name)}}
-                                        </td>
-                                    @else
-                                        <td class="px-4 py-4 text-center">
-                                            <span class="flex w-3 h-3 me-3 bg-blue-600 rounded-full"></span>
+@props(['align' => 1])
+@php
+    switch ($align) {
+        case 1:
+            $class = 'text-start';
+            break;
+        case 2:
+            $class = 'text-center';
+            break;
+        default:
+            # code...
+            break;
+    }
+@endphp
+<div>
+    <div class="relative overflow-x-auto">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400 mb-3">
+            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                    @foreach ($headers as $value)
+                        @if ($value != 'id')
+                            <th scope="col" {{$attributes->merge(['class' => 'px-4 py-3' . $class])}}>
+                                {{$value}}
+                            </th>
+                        @else
+                            <th scope="col" class="px-4 py-3 text-center"></th>
+                        @endif
+                    @endforeach
+                    <th class="px-4 py-3 text-center">
+                        Action
+                    </th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                    @foreach ($list as $value)
+                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                            @foreach ($headers as $name)
+                                @if ($name != 'id')
+                                    <td {{$attributes->merge(['class' => 'px-4 py-4' . $class])}}>
+                                        {{($value->$name)}}
+                                    </td>
+                                @else
+                                    <td class="px-4 py-4 text-center">
+                                        <span class="flex w-3 h-3 me-3 bg-blue-600 rounded-full"></span>
 
-                                        </td>
-                                    @endif
-                                @endforeach
-                                <td class="px-4 py-3 text-center">
-                                    <x-button :client="$value" color="blue">EDIT</x-button>
-                                    <x-delete-button :object="$value">{{$value->name}}</x-delete-button>
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tr>
-                </tbody>
-            </table>
-            {{$list->withQueryString()->links() }}
-        </div>
+                                    </td>
+                                @endif
+                            @endforeach
+                            <td class="px-4 py-3 text-center">
+                                <x-button :client="$value" color="blue">EDIT</x-button>
+                                <x-delete-button :object="$value">{{$value->name}}</x-delete-button>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tr>
+            </tbody>
+        </table>
+        {{$list->withQueryString()->links() }}
     </div>
+</div>
