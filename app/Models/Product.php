afab5073d2c5f417253440f35600e6e49e8fd1e7
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-03-27 09:11:40 +0100

    feat(product model): fillable and cast was implemented

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -10,6 +11,19 @@ class Product extends Model
     use HasFactory;
 
     protected $hidden = ['created_at', 'updated_at'];
+
+    protected $fillable = ['name', 'reference', 'stock'];
+
+    protected function reference(): Attribute{
+        return Attribute::make(
+            set: function($txt){
+                return strtoupper($txt);
+            },
+            get: function($txt){
+                return strtoupper($txt);
+            }
+        );
+    }
 
     public static function fileteredNames(array $headers){
         $filteredColumns = array_filter($headers, function($column) {
