958b8a93395192c3d42facee9b6c1d244e5045ab
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-03-27 12:35:12 +0100

    feat(clientProduct): index view and relationship created

--- app/Models/ClientProduct.php
+++ app/Models/ClientProduct.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class ClientProduct extends Model
 {
@@ -15,10 +16,29 @@ class ClientProduct extends Model
 
     protected $hidden = ['created_at', 'update_at'];
 
+    public function client():HasOne {
+        return $this->hasOne(Client::class, 'id', 'client_id');
+    }
+
+    public function product():HasOne {
+        return $this->hasOne(Product::class, 'id', 'product_id');
+    }
+
     public static function fileteredNames(array $headers){
         $filteredColumns = array_filter($headers, function($column) {
            return !in_array($column, ['created_at', 'updated_at']);
         });
         return $filteredColumns;
+    }
+
+    public static function namesChange($clients_list){
+        foreach($clients_list as $object){
+                $name_client_id = $object->client['name'];
+                $name_product_id = $object->product['name'];
+
+                $object->client_id = $name_client_id;
+                $object->product_id = $name_product_id;
+        }
+        return $clients_list;
     }
 }
---
+++ resources/views/clientsProducts/index.blade.php
@@ -0,0 +1,19 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight content-center">
+            {{ __('Clientes y Productos') }}
+        </h2>
+        <x-button-icon :route="route('clients.products.create')"></x-button-icon>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <x-search :route="route('clients.products.search')" :list="$names_list" :search="$input"></x-search>
+                    <x-table :headers="$names_list" :route_delete="'clients.products.destroy'" :route_edit="'clients.products.edit'" :list="$cliPro_list"></x-table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
