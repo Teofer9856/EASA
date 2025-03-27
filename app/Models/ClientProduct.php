be6797d31bf662976fb7fd5552fe82ea8f57fd8c
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-03-27 10:59:49 +0100

    feat(clientProduct model): variables and methos created

--- app/Models/ClientProduct.php
+++ app/Models/ClientProduct.php
@@ -10,4 +10,15 @@ class ClientProduct extends Model
     use HasFactory;
 
     protected $table = 'clients_products';
+
+    protected $fillable = ['name'];
+
+    protected $hidden = ['created_at', 'update_at'];
+
+    public static function fileteredNames(array $headers){
+        $filteredColumns = array_filter($headers, function($column) {
+           return !in_array($column, ['created_at', 'updated_at']);
+        });
+        return $filteredColumns;
+    }
 }
