6796a6820f5536fcfbea4dfa44e3a1d6ec33b07a
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-03-31 13:32:53 +0200

    fix(client sortable): province and seller method for sortable has been created

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -30,6 +30,20 @@ class Client extends Model
         return $this->belongsTo(Seller::class);
     }
 
+    public function provinceSortable($query, $province)
+    {
+        return $query->join('provinces', 'clients.province_id', '=', 'provinces.id')
+            ->orderBy('provinces.name', $province)
+            ->select('clients.*');
+    }
+
+    public function sellerSortable($query, $province)
+    {
+        return $query->join('sellers', 'clients.seller_id', '=', 'sellers.id')
+            ->orderBy('sellers.name', $province)
+            ->select('clients.*');
+    }
+
     public static function namesChange($clients_list){
         foreach($clients_list as $object){
             $name_province = $object->province['name'];
