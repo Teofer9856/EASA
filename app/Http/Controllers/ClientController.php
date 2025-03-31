80a5214c97fbd8b09ba9707f9cf3201d0186f26c
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-03-31 09:30:14 +0200

    fix(client search): search sortable has been integrated

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -52,12 +52,12 @@ class ClientController extends Controller
         $ids_sellers = Seller::where('name', 'like', "%$request->search%")->pluck('id');
 
         if($request->option != 'All'){
-            $clients_list = Client::namesChange(Client::orderBy('id', 'DESC')->where("$request->option", 'like', "%$request->search%")
+            $clients_list = Client::namesChange(Client::sortable()->where("$request->option", 'like', "%$request->search%")
             ->orWhere("$request->option", "=", Province::where("name", 'like', "%$request->search%")->value("id"))
             ->orWhereIn("$request->option", $ids_sellers)->paginate(15));
             $input = ['search' => $request->search, 'option' => $request->option];
         } else {
-            $clients_list = Client::namesChange(Client::orderBy('id', 'DESC')->paginate(15));
+            $clients_list = Client::namesChange(Client::sortable()->paginate(15));
             $input = ['search' => '', 'option' => ''];
         }
         $entity = Schema::getColumnListing('clients');
