7c828f58e5b5e5689685e07fdd158d9e0debe3a9
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-04-02 09:58:22 +0200

    build(select2): installion of jquery, and select2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,6 +16,23 @@
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
         <script src="https://kit.fontawesome.com/1f30b709a4.js" crossorigin="anonymous"></script>
+
+        {{-- Jquery, Select2 --}}
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />
+        <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+        <style>
+            .select2-selection__rendered {
+                line-height: 31px !important;
+                margin-top: 4px;
+            }
+            .select2-container .select2-selection--single {
+                height: 41px !important;
+            }
+            .select2-selection__arrow {
+                height: 34px !important;
+            }
+        </style>
         @stack('links')
     </head>
     <body class="font-sans antialiased">
@@ -36,6 +53,12 @@
                 {{ $slot }}
             </main>
             <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+            <script>
+                // In your Javascript (external .js resource or <script> tag)
+                $(document).ready(function() {
+                    $('.search').select2();
+                });
+            </script>
         </div>
     </body>
 </html>
