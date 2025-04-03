f0bf16eb18e8e33f4c4141976ec8abc4f318ccdd
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-04-03 14:59:55 +0200

    fix(buyers): province has been removed

--- resources/views/components/buyers.blade.php
+++ resources/views/components/buyers.blade.php
@@ -3,10 +3,9 @@
         <!-- head -->
         <thead>
             <tr>
-                <th></th>
+                <th>Nº</th>
                 <th>Name</th>
                 <th>Email</th>
-                <th>Province</th>
             </tr>
         </thead>
         <tbody>
@@ -14,16 +13,14 @@
             @if ($key == 0)
                 <tr class="bg-base-200">
                     <th>{{$key += 1}}</th>
-                    <td>{{strtok($buyer['name'], ' ')}}</td>
+                    <td>{{$buyer['name']}}</td>
                     <td>{{$buyer['email']}}</td>
-                    <td>{{strtok($buyer['province']['name'], explode(' ', $buyer['province']['name'])[2])}}</td>
                 </tr>
             @else
             <tr>
                 <th>{{$key += 1}}</th>
-                    <td>{{strtok($buyer['name'], ' ')}}</td>
+                    <td>{{$buyer['name']}}</td>
                     <td>{{$buyer['email']}}</td>
-                <td>{{strtok($buyer['province']['name'], explode(' ', $buyer['province']['name'])[2] ?? ' ')}}</td>
             </tr>
             @endif
             @endforeach
