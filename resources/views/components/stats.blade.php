3781c752a68083178675ca552a05b3651a4fbc6e
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-04-04 13:44:38 +0200

    style(dashboard): style was improved

--- resources/views/components/stats.blade.php
+++ resources/views/components/stats.blade.php
@@ -1,18 +1,18 @@
 <div class="stats w-full mt-8">
     <div class="stat place-items-center">
-        <div class="stat-title text-lg">Products</div>
+        <div class="stat-title text-xl">Products</div>
         <div class="stat-value text-4xl">{{ $stats[0] }}</div>
         <div class="stat-desc text-sm">{{ date('M d', strtotime('-1 month')) }} - {{ date('M d') }}</div>
     </div>
 
     <div class="stat place-items-center">
-        <div class="stat-title text-lg">New Clients</div>
+        <div class="stat-title text-xl">New Clients</div>
         <div class="stat-value text-success text-4xl">{{ $stats[1] }}</div>
         <div class="stat-desc text-success text-sm">Today</div>
     </div>
 
     <div class="stat place-items-center">
-        <div class="stat-title text-lg">Top Product</div>
+        <div class="stat-title text-xl">Top Product</div>
         <div class="stat-value">{{ number_format($stats[2], 2, ',', '.') }}€</div>
         <div class="stat-desc text-sm">Over all</div>
     </div>
