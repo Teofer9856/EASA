609630bc7d62cc34cb5bd63e7b556370ab9fd0dd
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-04-04 12:45:03 +0200

    feat(apiController): apiControllers was created and each of its methods

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -6,9 +6,16 @@ use App\Http\Requests\StoreClientRequest;
 use App\Http\Requests\UpdateClientRequest;
 use App\Models\Client;
 use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
 
-class ApiController extends Controller
+class ApiController extends Controller implements HasMiddleware
 {
+    public static function middleware()
+    {
+        return [new Middleware('auth:sanctum', except: ['index', 'show'])];
+    }
+
     /**
      * Display a listing of the resource.
      */
