3af044c9f33174e63fae9a7e1ebe963edd2fc16f
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-04-04 10:15:16 +0200

    build(api): api was created and implemented

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreClientRequest;
+use App\Http\Requests\UpdateClientRequest;
+use App\Models\Client;
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Client::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreClientRequest $request)
+    {
+        $client = Client::create($request->validated());
+
+        return ['client' => $client];
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Client $client)
+    {
+        return ['client' => $client];
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateClientRequest $request, Client $client)
+    {
+        $client->update($request->validated());
+
+        return ['client' => $client];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Client $client)
+    {
+        $client->delete();
+
+        return ['message' => 'The Client was exterminated successfully'];
+    }
+}
