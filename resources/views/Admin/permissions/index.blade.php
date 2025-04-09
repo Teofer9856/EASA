d0927c1f70c82610969560bfe4db98c963e42dec
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-04-09 16:42:49 +0200

    feat(permissions table): table for permissions created

---
+++ resources/views/Admin/permissions/index.blade.php
@@ -0,0 +1,52 @@
+<x-app-layout>
+    <x-alert>{{session('status')}}</x-alert>
+    @if ($errors->any())
+        <x-alert-list></x-alert-list>
+    @endif
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight content-center">
+            {{ __('Admin Permissions') }}
+        </h2>
+        <div class="flex">
+            {{-- <x-button-icon :route="route('clients.create')"></x-button-icon> --}}
+            <div class="dropdown dropdown-hover">
+                <div tabindex="0" role="button" class="btn btn-outline btn-neutral m-1 border border-black">Mas Opciones</div>
+                <ul tabindex="0" class="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                  <li><a href="{{ route('admin.roles.index') }}">Roles</a></li>
+                  <li><a href="{{ route('admin.permissions.index') }}">Permisos</a></li>
+                </ul>
+            </div>
+        </div>
+    </x-slot>
+
+    <div class="py-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    {{-- <x-search :list="$names_list" :route="route('clients.search')" class="mb-4" :search="$input"></x-search> --}}
+                    <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                          <tr>
+                            <th scope="col" class="px-6 py-3"></th>
+                            <th scope="col" class="px-6 py-3">Name</th>
+                            <th scope="col" class="px-6 py-3">Action</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          @foreach ($permissions as $permission)
+                              <tr class="hover:bg-base-300">
+                                  <td class="px-6 py-4"><div aria-label="success" class="status status-success"></div></td>
+                                  <td class="px-6 py-4">{{$permission->name}}</td>
+                                  <td class="px-6 py-4">
+                                      <x-button :client="$role" :route="'admin.permissions.edit'" color="blue">EDIT</x-button>
+                                      <x-delete-button :route="'admin.permissions.destroy'" :object="$role">{{$role->name ?? "Corrige Esto"}}</x-delete-button>
+                                  </td>
+                              </tr>
+                          @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
