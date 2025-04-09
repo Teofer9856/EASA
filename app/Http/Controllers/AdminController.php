99c42b1772e99cea4cedfe4d559e3836fd877608
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-04-09 13:13:15 +0200

    fix(admin index): content fixed

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,46 +9,17 @@ use App\Models\Seller;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 
 class AdminController extends Controller
 {
     function index(){
-        $firstDayMonth = Carbon::now()->startOfMonth()->toDateTimeString();
-        $lastDayOfMonth = Carbon::now()->endOfMonth()->toDateString();
-        $firstDayLastMonth = Carbon::now()->startOfMonth()->subMonth()->toDateTimeString();
-        $lastDayOfPreviousMonth = Carbon::now()->subMonth()->endOfMonth()->toDateString();
+        $clients_list = Client::namesChange(Client::sortable('name')->paginate(15));
+        $entity = Schema::getColumnListing('clients');
+        $names_list = array_splice($entity, 0,6);
+        $input = ['search' => '', 'option' => ''];
 
-        $data = [
-            count(Client::where([['created_at', '>=', $firstDayLastMonth], ['created_at', '<=', $lastDayOfPreviousMonth]])->get()),
-            count(Product::where([['created_at', '>=', $firstDayLastMonth], ['created_at', '<=', $lastDayOfPreviousMonth]])->get()),
-            count(Seller::where([['created_at', '>=', $firstDayLastMonth], ['created_at', '<=', $lastDayOfPreviousMonth]])->get()),
-            count(ClientProduct::where([['created_at', '>=', $firstDayLastMonth], ['created_at', '<=', $lastDayOfPreviousMonth]])->get())
-        ];
-
-        $stats = [
-            count(Product::select('id')->where('created_at', '>=', Carbon::now()->subDays(30)->toDateTimeString())->get()),
-            count(Client::select('id')->where('created_at', '>=', $firstDayMonth)->get()),
-            ClientProduct::max('price')
-        ];
-
-        /* !Revisa */
-        $topThreeIds = ClientProduct::select('client_id')->groupBy('client_id')->orderBy(DB::raw('max(price)'), 'desc')->take(3)->pluck('client_id');
-        $topThree = Client::whereIn('id', $topThreeIds)->orderBy('id', 'desc')->get();
-
-        $profit = [
-            'lastMonth' => $lastMonthSells = ClientProduct::select('price')->where([['created_at', '>=', $firstDayLastMonth], ['created_at', '<=', $lastDayOfPreviousMonth]])->sum('price'),
-            'thisMonth' => $monthSells = ClientProduct::select('price')->where('created_at', '>=', $firstDayMonth)->sum('price')
-        ];
-
-        $mostSelled = ClientProduct::select('product_id', ClientProduct::raw('COUNT(*) as cantidad'))->groupBy('product_id')->orderBy('cantidad', 'DESC')->limit(1)->first();
-        $name = Product::where('id', $mostSelled->product_id)->pluck('name');
-        $price = number_format(ClientProduct::where('product_id', $mostSelled->product_id)->sum('price'), 0, ',', '.');
-
-        $mostSelledList = [
-            'name' => $name[0], 'id' => $mostSelled->product_id,
-            'price' => $price, 'total' => $mostSelled->cantidad
-        ];
-
-        return view('admin.index', compact('data', 'stats', 'topThree', 'profit', 'mostSelledList'));
+        $success = session('success');
+        return view('admin.index', compact(['clients_list', 'names_list', 'input', 'success']));
     }
 }
--- resources/views/Admin/index.blade.php
+++ resources/views/Admin/index.blade.php
@@ -1,71 +1,25 @@
-@push('links')
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5" rel="stylesheet" type="text/css" />
-    <link href="https://cdn.jsdelivr.net/npm/daisyui@5/themes.css" rel="stylesheet" type="text/css" />
-    <style>
-        .tooltip .tooltiptext {
-            width: 150px;
-            height: 25px;
-            top: 101%;
-            left: 43%;
-            margin-left: -33px;
-            background-color: black;
-            color: white;
-        }
-    </style>
-@endpush
 <x-app-layout>
+    <x-alert>{{session('status')}}</x-alert>
+    @if ($errors->any())
+        <x-alert-list></x-alert-list>
+    @endif
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight content-center">
+            {{ __('Admin') }}
         </h2>
+        <div class="flex">
+            {{-- <x-button-icon :route="route('clients.create')"></x-button-icon> --}}
+        </div>
     </x-slot>
 
     <div class="py-8">
-        <div class="max-w mx-auto sm:px-6 lg:px-8">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <div class="flex-grow p-6 overflow-auto bg-gray-100">
-                        <div class="grid grid-cols-3 gap-6">
-                            <div class="h-50 col-span-1 bg-white border rounded-lg border-gray-300 tooltip">
-                                <span class="tooltiptext">Top Buyers</span>
-                                <x-buyers :buyers="$topThree"></x-buyers>
-                            </div>
-                            <div class="h-50 col-span-1 bg-white border rounded-lg border-gray-300">
-                                <x-profits :profits="$profit"></x-profits>
-                            </div>
-                            <div class="h-50 col-span-1 bg-white border rounded-lg border-gray-300">
-                                <x-stats :stats="$stats"></x-stats>
-                            </div>
-                            <div class="h-95 col-span-2 bg-white border rounded-lg border-gray-300">
-                                <canvas id="myChart" data-chart="{{json_encode($data)}}"></canvas>
-                            </div>
-                            <div class="h-95 col-span-1 bg-white border rounded-lg border-black-300">
-                                <div class="stat place-items-center mt-3">
-                                    <div class="stat-value text-1xl text-primary text-4xl mt-1">Most Sold</div>
-                                    <div class="stat mt-2">
-                                        <div class="stat-value text-success text-8xl text-center">{{$mostSelledList['total']}}</div>
-                                        <div class="stat-value text-success text-6xl text-center"><span class="text-success text-5xl">
-                                            <div>
-                                                {{$mostSelledList['name']}}
-                                            </div>
-                                            <div class="mt-3">
-                                                {{$mostSelledList['price']}} €
-                                            </div>
-                                    </div>
-                                </div>
-                            </div>
-
-                        </div>
-                    </div>
+                    <x-search :list="$names_list" :route="route('clients.search')" class="mb-4" :search="$input"></x-search>
+                    <x-table :headers="$names_list" :route_delete="'clients.destroy'" :route_edit="'clients.edit'" :list="$clients_list"></x-table>
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script type="module" src="https://unpkg.com/cally"></script>
-@vite('resources/js/chart.js')
