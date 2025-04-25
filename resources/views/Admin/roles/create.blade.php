4ca83d706a4d10d21da900aa485af6e31f7d070f
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-04-25 13:21:46 +0200

    feat(multi select): select of permissions created

--- resources/views/Admin/roles/create.blade.php
+++ resources/views/Admin/roles/create.blade.php
@@ -19,6 +19,18 @@
                                     <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Oops!</span> {{$message}}</p>
                                 @enderror
                             </div>
+                            <div class="relative z-0 w-full mb-5 group">
+                                <label for="permissions" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Permisos</label>
+                                <select multiple id="permissions" name="permissions[]" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    <option disabled>Elije los permisos</option>
+                                    @foreach ($permissions as $permission)
+                                        <option value="{{ $permission->name }}">{{$permission->name}}</option>
+                                    @endforeach
+                                </select>
+                                @error('permissions')
+                                    <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-medium">Oops!</span> {{$message}}</p>
+                                @enderror
+                            </div>
 
                             <div class="grid md:grid-cols-2 md:gap-6">
                                 <div class="relative z-0 w-full mb-5 group">
