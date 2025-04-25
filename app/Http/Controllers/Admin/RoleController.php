7a91d5a107ea3556b1012de69ef35bd7d94cbe2c
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-04-25 13:20:59 +0200

    fix(role controller): now it can update or create a role with an specific permission

--- app/Http/Controllers/Admin/RoleController.php
+++ app/Http/Controllers/Admin/RoleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RoleController extends Controller
@@ -15,14 +16,17 @@ class RoleController extends Controller
     }
 
     function create(){
-        return view('admin.roles.create');
+        $permissions = Permission::all();
+        return view('admin.roles.create', compact('permissions'));
     }
 
     function store(Request $role){
         $role->validate([
             'name' => 'required|string|max:255|unique:roles,name',
+            'permissions' => 'array',
         ]);
-        Role::create(['name' => strtolower($role->name)]);
+        $rol = Role::create(['name' => strtolower($role->name)]);
+        $rol->syncPermissions($role->permissions);
         return redirect()->route('admin.roles.index')->with('status', 'Create Rol: El rol se ha creado correctamente');
     }
 
@@ -33,13 +37,16 @@ class RoleController extends Controller
 
     public function edit(Role $role)
     {
-        return view('admin.roles.edit', compact('role'));
+        $permissions = Permission::all();
+        return view('admin.roles.edit', compact('role', 'permissions'));
     }
 
     function update(Request $request, Role $role){
         $request->validate([
-            'name' => 'required|string|max:255|unique:roles,name',
+            'name' => "required|string|max:255|unique:roles,name,{$role->id}",
+            'permissions' => 'array',
         ]);
+        $role->syncPermissions($request->permissions);
         $role->update(['name' => strtolower($request->name)]);
         return redirect()->route('admin.roles.index')->with('status', 'Update Rol: El rol se ha actualizado correctamente');
     }
