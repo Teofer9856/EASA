cd84c88256eeff32eafe309aa3fcab08e600ac91
Christopher Ramirez <christopher.ramirez@softeca.es>
2025-04-25 14:10:02 +0200

    feat(roles button): roles button component created

---
+++ resources/views/components/roles-button.blade.php
@@ -0,0 +1,31 @@
+@props(['color' => 'blue', 'client', 'route'])
+
+@php
+    switch ($color) {
+        case 'blue':
+            $class = "px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 tooltip";
+            break;
+        case 'red':
+            $class = "px-3 py-2 text-xs font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800 tooltip";
+            break;
+        case 'yellow':
+            $class = "px-3 py-2 text-xs font-medium text-center text-white bg-yellow-600 rounded-lg hover:bg-yellow-800 focus:ring-4 focus:outline-none focus:ring-yellow-300 dark:bg-yellow-600 dark:hover:bg-yellow-700 dark:focus:ring-yellow-800 tooltip";
+            break;
+        default:
+            $class = "px-3 py-2 text-xs font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 tooltip";
+            break;
+    }
+@endphp
+
+<span>
+    <a href="{{route($route, $client)}}">
+        <button {{$attributes->merge(['class' => $class])}}>
+            <span class="tooltiptext">Roles</span>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="black" class="h-5 w-5" viewBox="0 0 52 52" data-name="Layer 1">
+                <path d="M38.3,27.2A11.4,11.4,0,1,0,49.7,38.6,11.46,11.46,0,0,0,38.3,27.2Zm2,12.4a2.39,2.39,0,0,1-.9-.2l-4.3,4.3a1.39,1.39,0,0,1-.9.4,1,1,0,0,1-.9-.4,1.39,1.39,0,0,1,0-1.9l4.3-4.3a2.92,2.92,0,0,1-.2-.9,3.47,3.47,0,0,1,3.4-3.8,2.39,2.39,0,0,1,.9.2c.2,0,.2.2.1.3l-2,1.9a.28.28,0,0,0,0,.5L41.1,37a.38.38,0,0,0,.6,0l1.9-1.9c.1-.1.4-.1.4.1a3.71,3.71,0,0,1,.2.9A3.57,3.57,0,0,1,40.3,39.6Z"/>
+                <circle cx="21.7" cy="14.9" r="12.9"/>
+                <path d="M25.2,49.8c2.2,0,1-1.5,1-1.5h0a15.44,15.44,0,0,1-3.4-9.7,15,15,0,0,1,1.4-6.4.77.77,0,0,1,.2-.3c.7-1.4-.7-1.5-.7-1.5h0a12.1,12.1,0,0,0-1.9-.1A19.69,19.69,0,0,0,2.4,47.1c0,1,.3,2.8,3.4,2.8H24.9C25.1,49.8,25.1,49.8,25.2,49.8Z"/>
+            </svg>
+        </button>
+    </a>
+</span>
