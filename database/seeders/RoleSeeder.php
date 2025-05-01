7afff2a3c4500aa29906b1c31213fe4e7690b14d
Teofer9856 <spiderpoli9856@gmail.com>
2025-05-01 18:56:11 +0200

    fix(role seeder): name plural changed to singular

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -14,6 +14,6 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         Role::create(['name' => 'admin']);
-        Role::create(['name' => 'workers'])->givePermissionTo(['ver']);
+        Role::create(['name' => 'worker'])->givePermissionTo(['ver']);
     }
 }
