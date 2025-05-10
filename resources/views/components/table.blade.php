1de55759430095d76c0da4b3ff77d31190d7b8f5
Teofer9856 <spiderpoli9856@gmail.com>
2025-05-10 18:15:23 +0200

    style(table): for clients adapted

--- resources/views/components/table.blade.php
+++ resources/views/components/table.blade.php
@@ -19,26 +19,33 @@
                 <tr>
                     @foreach ($headers as $value)
                         @if ($value != 'id')
-                            <th scope="col" {{$attributes->merge(['class' => 'px-4 py-3' . $class])}}>
-                                @switch($value)
-                                    @case($value == "province_id")
-                                        @sortablelink('province')
-                                        @break
-                                    @case($value == "seller_id")
-                                        @sortablelink('seller')
-                                        @break
-                                    @case($value == "client_id")
-                                        @sortablelink('client')
-                                        @break
-                                    @case($value == "product_id")
-                                        @sortablelink('product')
-                                        @break
-                                    @default
-                                        @sortablelink($value)
-                                @endswitch
-                            </th>
+                            @if ($value == 'zip_code')
+                                <th scope="col" {{$attributes->merge(['class' => 'hidden xl:table-cell px-4 py-3 ' . $class])}}>
+                                    @sortablelink($value)
+                                </th>
+                                @elseif ($value == 'province_id')
+                                <th scope="col" {{$attributes->merge(['class' => 'hidden lg:table-cell px-4 py-3 ' . $class])}}>
+                                    @sortablelink('province')
+                                </th>
+                            @else
+                                <th scope="col" {{$attributes->merge(['class' => 'px-4 py-3 ' . $class])}}>
+                                    @switch($value)
+                                        @case($value == "seller_id")
+                                            @sortablelink('seller')
+                                            @break
+                                        @case($value == "client_id")
+                                            @sortablelink('client')
+                                            @break
+                                        @case($value == "product_id")
+                                            @sortablelink('product')
+                                            @break
+                                        @default
+                                            @sortablelink($value)
+                                    @endswitch
+                                </th>
+                            @endif
                         @else
-                            <th scope="col" class="px-4 py-3 text-start">⊛</th>
+                            <th scope="col" class="px-4 py-3 text-start hidden xl:block">⊛</th>
                         @endif
                     @endforeach
                     @canany(['editar', 'eliminar'])
@@ -49,49 +56,56 @@
                 </tr>
             </thead>
             <tbody>
-                <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                    @foreach ($list as $value)
-                        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
-                            @foreach ($headers as $name)
-                                @if ($name != 'id')
-                                    <td {{$attributes->merge(['class' => 'px-4 py-4' . $class])}}>
-                                        {{($value->$name)}}
-                                    </td>
-                                @else
-                                    <td class="px-4 py-4 text-center">
-                                        <span class="flex w-3 h-3 me-3 bg-blue-600 rounded-full"></span>
-
-                                    </td>
-                                @endif
-                            @endforeach
-                            @role('admin')
-                                <td class="px-4 py-3 text-center">
-                                    <x-button :client="$value" :route="$route_edit" color="blue">EDIT</x-button>
-                                    <x-delete-button :route="$route_delete" :object="$value">{{$value->name ?? $value->id}}</x-delete-button>
+                @foreach ($list as $value)
+                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
+                        @foreach ($headers as $name)
+                            @if ($name == 'zip_code')
+                                <td {{$attributes->merge(['class' => 'hidden xl:table-cell px-4 py-4 ' . $class])}}>
+                                    {{ $value->zip_code }}
+                                </td>
+                            @elseif( $name == 'province_id')
+                                <td {{$attributes->merge(['class' => 'hidden lg:table-cell px-4 py-4 ' . $class])}}>
+                                    {{ $value->$name }}
                                 </td>
                             @else
-                                @can('editar')
-                                    @can('eliminar')
-                                        <td class="px-4 py-3 text-center">
-                                            <x-button :client="$value" :route="$route_edit" color="blue">EDIT</x-button>
-                                            <x-delete-button :route="$route_delete" :object="$value">{{$value->name ?? $value->id}}</x-delete-button>
-                                        </td>
-                                    @else
-                                        <td class="px-4 py-3 text-center">
-                                            <x-button :client="$value" :route="$route_edit" color="blue">EDIT</x-button>
-                                        </td>
-                                    @endcan
+                                @if ($name != 'id')
+                                    <td {{$attributes->merge(['class' => 'px-4 py-4 ' . $class])}}>
+                                        {{ $value->$name }}
+                                    </td>
                                 @else
-                                    @can('eliminar')
-                                        <td class="px-4 py-3 text-center">
-                                            <x-delete-button :route="$route_delete" :object="$value">{{$value->name ?? $value->id}}</x-delete-button>
-                                        </td>
-                                    @endcan
+                                    <td class="px-4 py-4 text-center hidden xl:block">
+                                        <span class="flex w-3 h-3 me-3 bg-blue-600 rounded-full"></span>
+                                    </td>
+                                @endif
+                            @endif
+                        @endforeach
+                        @role('admin')
+                            <td class="px-4 py-3 text-center">
+                                <x-button :client="$value" :route="$route_edit" color="blue">EDIT</x-button>
+                                <x-delete-button :route="$route_delete" :object="$value">{{$value->name ?? $value->id}}</x-delete-button>
+                            </td>
+                        @else
+                            @can('editar')
+                                @can('eliminar')
+                                    <td class="px-4 py-3 text-center">
+                                        <x-button :client="$value" :route="$route_edit" color="blue">EDIT</x-button>
+                                        <x-delete-button :route="$route_delete" :object="$value">{{$value->name ?? $value->id}}</x-delete-button>
+                                    </td>
+                                @else
+                                    <td class="px-4 py-3 text-center">
+                                        <x-button :client="$value" :route="$route_edit" color="blue">EDIT</x-button>
+                                    </td>
                                 @endcan
-                            @endrole
-                        </tr>
-                    @endforeach
-                </tr>
+                            @else
+                                @can('eliminar')
+                                    <td class="px-4 py-3 text-center">
+                                        <x-delete-button :route="$route_delete" :object="$value">{{$value->name ?? $value->id}}</x-delete-button>
+                                    </td>
+                                @endcan
+                            @endcan
+                        @endrole
+                    </tr>
+                @endforeach
             </tbody>
         </table>
         {{$list->withQueryString()->links() }}
