c2c2ba03db7eff35b2035bd29ef00517b71b47a2
Teofer9856 <spiderpoli9856@gmail.com>
2025-05-10 14:32:05 +0200

    style(stats comp): overflow hidden

--- resources/views/components/stats.blade.php
+++ resources/views/components/stats.blade.php
@@ -1,17 +1,17 @@
-<div class="stats w-full mt-8">
-    <div class="stat place-items-center">
+<div class="stats w-full mt-9 overflow-hidden ">
+    <div class="stat place-items-center w-full">
         <div class="stat-title text-xl">Products</div>
         <div class="stat-value text-4xl">{{ $stats[0] }}</div>
         <div class="stat-desc text-base">{{ date('M d', strtotime('-1 month')) }} - {{ date('M d') }}</div>
     </div>
 
-    <div class="stat place-items-center">
+    <div class="stat place-items-center w-full">
         <div class="stat-title text-xl">New Clients</div>
         <div class="stat-value text-success text-4xl">{{ $stats[1] }}</div>
         <div class="stat-desc text-success text-base">Month</div>
     </div>
 
-    <div class="stat place-items-center">
+    <div class="stat place-items-center w-full">
         <div class="stat-title text-xl">Top Product</div>
         <div class="stat-value">{{ number_format($stats[2], 0, ',', '.') }}€</div>
         <div class="stat-desc text-base">Over all</div>
